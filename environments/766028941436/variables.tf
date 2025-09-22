e167f04d174b06a85ec4a0b346599965bc4613d3
cristopher sage <csage@teco.com.ar>
2025-09-22 17:11:33 -0300

    pruebas de github

--- environments/766028941436/variables.tf
+++ environments/766028941436/variables.tf
@@ -1,7 +1,9 @@
 variable "bucket_name" {
   description = "Nombre del bucket de S3"
   type        = string
+  default     = "mi-bucket-de-prueba-terraform"
 }
+
 
 variable "aws_region" {
   description = "Región de AWS"
