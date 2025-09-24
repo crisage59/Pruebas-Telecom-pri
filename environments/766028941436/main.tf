8893d48fbe432d88a257bcca1ed1665cae1a1e46
cristopher sage <csage@teco.com.ar>
2025-09-24 14:02:58 -0300

    pruebas de github

--- environments/766028941436/main.tf
+++ environments/766028941436/main.tf
@@ -6,8 +6,6 @@ resource "aws_s3_bucket" "example" {
   bucket        = var.bucket_name
   force_destroy = true
 
-
-
   tags = {
     Environment = var.environment
     Name        = var.bucket_name
